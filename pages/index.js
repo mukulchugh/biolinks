e839e9c922409e7c68d61732d475741c1f1df75e
Mukul Chugh <15247594+mukulchugh@users.noreply.github.com>
2021-03-03 23:38:12 +0530

    First commit

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Mukul Chugh</title>
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+
+      <main>
+        <img
+          className="image"
+          src="https://mukulchugh.com/projects/profile.png"
+        />
+
+        <a href="https://instagram.com/themukulchugh">
+          <h3 className="description"> @TheMukulChugh</h3>
+        </a>
+
+        <div className="grid">
+          <a href="https://mukulchugh.com" className="card">
+            <h3>Portfolio</h3>
+          </a>
+
+          <a href="https://digitalmoshai.com" className="card">
+            <h3>Digital Moshai</h3>
+          </a>
+
+          <a href="https://www.linkedin.com/in/mukulchugh/" className="card">
+            <h3>Connect on LinkedIn</h3>
+          </a>
+
+          <a href="https://twitter.com/themukulchugh" className="card">
+            <h3>Follow me on Twitter</h3>
+          </a>
+
+          <a href="https://github.com/mukulchugh" className="card">
+            <h3>Github</h3>
+          </a>
+        </div>
+      </main>
+
+      <style jsx>{`
+       ...
+     `}</style>
+    </div>
+  );
+}
